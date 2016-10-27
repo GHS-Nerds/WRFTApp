d2aa3b996609d078378fcbe04bdcad07268f2d98
GHSNerds <GHSNerds@gmail.com>
2016-10-27 13:20:45 +0100

    Update and rename init.py to main.py
    based on work at https://github.com/stocyr/Deflectouch/blob/master/main.py Opens the app with useful libraries imported

---
+++ main.py
@@ -0,0 +1,20 @@
+import kivy
+kivy.require('1.8.0')
+
+from kivy.app import App
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty
+from kivy.factory import Factory
+from kivy.utils import boundary
+from kivy.clock import Clock
+from kivy.animation import Animation
+from kivy.core.audio import SoundLoader
+
+from kivy.uix.image import Image
+from kivy.uix.widget import Widget
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+
+TestApp().run()
