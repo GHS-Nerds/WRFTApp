f83a05cd2565050fc9a822bbbc7ccdd942904629
GHSNerds <GHSNerds@gmail.com>
2016-11-27 17:31:09 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -18,25 +18,6 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-sm = ScreenManager()
-catchRecord_screen = Screen(name='catchRecord')
-sm.add_widget(catchRecord_screen)
-
-class home_screen(Screen):
-    '''
-    This is where the setting up for buttons for the home screen goes
-    '''
-    pass
-
-class catchRecord_screen(Screen):
-    '''
-    This is where the setting up for buttons for the home screen goes
-    '''
-    pass
-
-
-sm.add_widget(home_screen(name='home'))
-sm.add_widget(catchRecord_screen(name='home'))
 
 class WRFT(App):
     def build(self):
@@ -58,17 +39,9 @@ class WRFT(App):
     def weatherForcast_Detect(self, touch):
         -pass
     
+    def fishingLocations(self, Button):
+        return 
     
-class WeatherForecast(App):
-    def build(self):
-        return Button(text='Weather Forecast')
-
-class FishingLocations(App):
-    def build(self):
-        return Button(text='Local Fishing Locations')
-
-sm.add_widget(home_screen(name='home'))
-sm.add_widget(catchRecord_screen(name='home'))
 
 if __name__ == '__main__':
     WRFT().run()
