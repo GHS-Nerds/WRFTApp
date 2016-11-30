4abe23f4d0ddc43dc2bdf5eff6cc3778ea59225f
GHSNerds <GHSNerds@gmail.com>
2016-11-30 12:41:56 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,39 @@
 import kivy
-#kivy.require('1.8.0')
-
-from kivy.app import App
-from kivy.properties import ObjectProperty, StringProperty, NumericProperty
-from kivy.factory import Factory
-from kivy.utils import boundary
-from kivy.clock import Clock
-from kivy.animation import Animation
-from kivy.core.audio import SoundLoader
-
-from kivy.uix.image import Image
-from kivy.uix.widget import Widget
-from kivy.uix.popup import Popup
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+kivy.require('1.8.0')
+from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivy.uix.textinput import TextInput
+
+class rootWidget(BoxLayout):
+    fishName = ObjectProperty()   
+    pass 
 
 
-class WRFT(App):
+class WRFTApp(App):
     def build(self):
-        return Button(text='Record your catch')
-    
-    def on_touch_down(self, touch):
-    if self.collide_point(*touch.pos):
-        class catchRecord(BoxLayout):
+        return rootWidget()       
 
-    txt_inpt = ObjectProperty(None)
+if __name__=="__main__":
+    WRFTApp().run()
 
-    def check_status(self, btn):
-        print('button state is: {state}'.format(state=btn.state))
-        print('text input text is: {txt}'.format(txt=self.txt_inpt))
-        pass
-    
-    def recordCatch_process(self, catchList):
-        """
-        Clear screen
-        Go to screen with Collection points for Weight, photo, name of fish etc [Populate screen]
-        Input the output of them to a Dictionary
-        """
-        
-    def out2Database(self, catchList, serverNo):
-        '''
-        Change data from databases to outputtable form
-        Send data to database
-        '''
-    
-    def weatherForcast_Detect(self, Button):
-        pass
-    
-    def fishingLocations(self, Button):
-        pass
-    
-    def catchRecord(self, Button):
-        pass
-    
-   
 
-if __name__ == '__main__':
-    WRFT().run()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
