d044bf1b410194ae2cedc3ed245f1bb38507cf07
Jack Wright <fiosaiche@protonmail.com>
2017-03-08 22:54:48 +0000

    Delete browsermode.py

--- WRFTApp_email_tests/Browsermode/browsermode.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import webbrowser
-
-userinput = input('Yes or No: ')
-
-if userinput == 'Yes':
-    webbrowser.open_new("file:///path/to/WRFTApp_email_tests/Browsermode/form.cfm")
