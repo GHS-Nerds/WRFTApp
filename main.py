8c3e9f1db31e75ffb585ee04861e843c34fb6996
HTMLMacrae <rwmacrae@googlemail.com>
2017-03-26 22:20:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,56 @@ class SettingsScreen(Screen):
 class RecordCatchScreen(Screen):
     def sendDatabase():
         print'sent'
+
+    def fishWeightCollection(self):
+        fishWeight = ObjectProperty(None)
+        print (fishWeightIn) #DEBUG
+        fishWeightTypeInt = type(fishWeightIn) is int
+        fishWeightTypeFloat = type(fishWeightIn) is float
+        if fishWeightTypeInt != True or fishWeightTypeFloat != True: 
+            #If the weight of the fish is not a number do this
+            print('Entered weight is not a number', fishWeight) #Error Message 
+        elif fishWeightTypeInt == True or fishWeightTypeFloat == True:
+            #If the fish weight is a number do this
+            return fishWeightIn
+
+    def fishLengthCollection(self):
+        fishLengthIn = ObjectProperty(None)
+        print (fishLengthIn) #DEBUG
+        fishLengthTypeInt = type(fishLengthIn) is int
+        fishLengthTypeFloat = type(fishLengthIn) is float
+        if fishLengthTypeInt != True or fishLengthTypeFloat != True: 
+            #If the length of the fish is not a number do this
+            print('Entered length is not a number', fishLength) #Error Message 
+        elif fishLengthTypeInt == True or fishLengthTypeFloat == True:
+            #If the fish length is a number do this
+            return fishLengthIn   
+    '''
+    Information to be sent to server need to be packaged in the following order:
+    forename as fNameIn
+    surname as sNameIn
+    fish species as fishSpeciesIn
+    fish weight as fishWeightIn
+    fish length as fishLengthIn
+    '''
+
+    def csvPackage(fNameIn, sNameIn, fishSpeciesIn, fishWeightIn, fishLengthIn):
+        fName = ['f_name', fNameIn]
+        sName = ['s_name', sNameIn]
+        fishSpecies = ['fishName', fishSpeciesIn]
+        fishWeight = ['fishWeight', fishWeightIn]  
+        fishLength = ['fishLength', fishLengthIn]
+        
+        with open('WRFT.csv', 'w', newline='') as csvfile:
+            lineWriter = csv.writer(csvfile, delimiter=' ',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            lineWriter.writerow(fName)
+            lineWriter.writerow(sName)
+            lineWriter.writerow(fishSpecies)
+            lineWriter.writerow(fishWeight)
+            lineWriter.writerow(fishLength)
+            #Call the below to change the specified information
+            #csvPackage(fName, sName, fishSpecies, fishWeight, fishLength)
     pass
 
 class ScreenManagement(ScreenManager):
