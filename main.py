2b1cffb29b08a6e9e070f675fc103ed005949950
Roddy MacRae <rwmacrae@googlemail.com>
2017-06-26 16:20:57 +0100

    Added C

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.properties import ObjectProperty
 from kivy.properties import NumericProperty
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 import plyer
-import csv
+import pandas
 
 lochNames = ['Loch Tollie', 'Loch Bad an Sgalaig', 'Dubh Loch', 'Loch Garbhaig', 'Loch nam Breac', 'Loch Feur', 'Loch a\' Gharbh-doire', 'Loch Airigh Mhic Criadh', 'Loch Airigh a\' Phuill', 'Loch nam Buainichean', 'Loch na Feithe Mugaig', 'Loch Doire na h-Airighe', 'Loch an Aird-sheilg']
 
@@ -21,13 +22,8 @@ class WeatherScreen(Screen):
 class MapScreen(Screen):
     pass
 
+
 class SettingsScreen(Screen):
-    '''
-    DEBUG
-    '''
-    fNameIn = 'Roddy'
-    sNameIn = 'MacRae'
-    fishSpeciesIn = 'Trout'
 
     def wifiState(self):
         print('###')
@@ -35,72 +31,20 @@ class SettingsScreen(Screen):
     pass
 
 class RecordCatchScreen(Screen):
+    fishWeightIn = ObjectProperty(None)
+    fishLengthIn = ObjectProperty(None)
+
+    def sendDatabase():
+        #exportToDatabase(csvPackage(name, fishSpecies, fishWeight, fishLength))
+        #get the output from csvPackage() and use Jacks email script to send it
+        pass
+
     def exportToDatabase():
         ''''
         This is where Jacks email code will go
         '''
         pass
 
-    def fishName():
-        fishNames = ['Brown Trout', 'Sea Trout', 'Pike', 'Salmon', 'Charr', 'Arctic Charr']
-        #Need kivy script from Sean to finish this
-        #Plan is for a drop down however if Sean thinks up anythong different he is welcome to it
-
-    def fishWeightCollection(self):
-        fishWeightIn = NumericProperty(None)
-        print (fishWeightIn) #DEBUG
-        fishWeightTypeInt = type(fishWeightIn) is int
-        fishWeightTypeFloat = type(fishWeightIn) is float
-        if fishWeightTypeInt != True or fishWeightTypeFloat != True:
-            #If the weight of the fish is not a number do this
-            print('Entered weight is not a number', fishWeightIn) #Error Message
-        elif fishWeightTypeInt == True or fishWeightTypeFloat == True:
-            #If the fish weight is a number do this
-            return fishWeightIn
-
-    def fishLengthCollection(self):
-        fishLengthIn = NumericProperty(None)
-        print (fishLengthIn) #DEBUG
-        fishLengthTypeInt = type(fishLengthIn) is int
-        fishLengthTypeFloat = type(fishLengthIn) is float
-        if fishLengthTypeInt != True or fishLengthTypeFloat != True:
-            #If the length of the fish is not a number do this
-            print('Entered length is not a number', fishLengthIn) #Error Message
-        elif fishLengthTypeInt == True or fishLengthTypeFloat == True:
-            #If the fish length is a number do this
-            return fishLengthIn
-    '''
-    Information to be sent to server need to be packaged in the following order:
-    forename as fNameIn
-    surname as sNameIn
-    fish species as fishSpeciesIn
-    fish weight as fishWeightIn
-    fish length as fishLengthIn
-    This is important for translation on server side
-    '''
-
-    def csvPackage(self, NameIn, fishSpeciesIn, fishWeightIn, fishLengthIn):
-
-        Name = ['name', NameIn]
-        fishSpecies = ['fishName', fishSpeciesIn]
-        fishWeight = ['fishWeight', fishWeightIn]
-        fishLength = ['fishLength', fishLengthIn]
-
-        with open('WRFT.csv', 'w', newline='') as csvfile:
-            lineWriter = csv.writer(csvfile, delimiter=' ',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            lineWriter.writerow(fName)
-            lineWriter.writerow(sName)
-            lineWriter.writerow(fishSpecies)
-            lineWriter.writerow(fishWeight)
-            lineWriter.writerow(fishLength)
-            #Call the below to change the specified information
-            #csvPackage(fName, sName, fishSpecies, fishWeight, fishLength)
-
-    def sendDatabase():
-        exportToDatabase(csvPackage(fName, sName, fishSpecies, fishWeight, fishLength))
-        #get the output from csvPackage() and use Jacks email script to send it
-        print('sent')
     pass
 
 class ScreenManagement(ScreenManager):
@@ -109,6 +53,32 @@ class ScreenManagement(ScreenManager):
 kivyFile = Builder.load_file("main.kv")
 
 class MainApp(App):
+    index = 0
+    '''
+    Information to be sent to server need to be packaged in the following order:
+    full name as nameIn
+    fish species as fishSpeciesIn
+    fish weight as fishWeightIn
+    fish length as fishLengthIn
+    This is important for translation on server side
+    '''
+    def csvPackage(self, nameIn, fishSpeciesIn, fishWeightIn, fishLengthIn, index):
+        finalData = {
+            'index': [0],
+            'nameIn':nameIn,
+            'fishSpeciesIn':fishSpeciesIn,
+            'fishWeightIn':fishWeightIn,
+            'fishLengthIn':fishLengthIn
+        }
+        df = pandas.DataFrame(finalData)
+        df = df.set_index('index')
+        df.to_csv('C:/Users/rwmac/Documents/Programming/WRFTApp/InProgress/WRFT.csv')
+        print('\nDataFrame\n', df)
+        #Find the last index of the df and make the next index
+        index = (df.tail(1).index)+1
+        print('\n',index, '\n')
+        return df
+        return index
 
     def build(self):
         return kivyFile
