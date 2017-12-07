e8f97530771e9ff7f3ed553ef9f5362cb5cdce03
Roddy MacRae <rwmacrae@googlemail.com>
2017-12-07 22:34:10 +0000

    Add files via upload

---
+++ PCWorking/Work In Progress/main.py
@@ -0,0 +1,152 @@
+import kivy
+from kivy.app import App
+#kivy.require("1.8.0")
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.properties import ObjectProperty
+from kivy.properties import NumericProperty
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+import numpy as np
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
+from kivy.core.window import Window
+from kivy.graphics import Color
+import csv
+
+
+
+lochNames = ['Loch Tollie', 'Loch Bad an Sgalaig', 'Dubh Loch', 'Loch Garbhaig', 'Loch nam Breac', 'Loch Feur', 'Loch a\' Gharbh-doire', 'Loch Airigh Mhic Criadh', 'Loch Airigh a\' Phuill', 'Loch nam Buainichean', 'Loch na Feithe Mugaig', 'Loch Doire na h-Airighe', 'Loch an Aird-sheilg']
+
+class HomeScreen(Screen):
+    pass
+
+class WeatherScreen(Screen):
+    pass
+
+class MapScreen(Screen):
+    pass
+
+class InvaScreen(Screen):
+    pass
+
+class InfoScreen(Screen):
+    pass
+
+class NonInvaScreen(Screen):
+    pass
+
+class LicencesScreen(Screen):
+    pass
+
+class LampreysScreen(Screen):
+    pass
+
+class EelScreen(Screen):
+    pass
+
+class MinnowScreen(Screen):
+    pass
+
+class SticklebackScreen(Screen):
+    pass
+
+class SalmonScreen(Screen):
+    pass
+
+class TroutScreen(Screen):
+    pass
+
+class ArcticCharrScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+
+    def wifiState(self):
+        print('###')
+        #Get the status and check if the wifi is active and has network access
+    pass
+
+class RecordCatchScreen(Screen):
+    fishWeightIn = ObjectProperty(None)
+    fishLengthIn = ObjectProperty(None)
+
+    def sendDatabase():
+        #exportToDatabase(csvPackage(name, fishSpecies, fishWeight, fishLength))
+        #get the output from csvPackage() and use Jacks email script to send it
+        pass
+
+    def exportToDatabase():
+        ''''
+        This is where Jacks email code will go
+        '''
+        pass
+
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+kivyFile = Builder.load_file("main.kv")
+
+class MainApp(App):
+    index = 0
+    '''
+    Information to be sent to server need to be packaged in the following order:
+    full name as nameIn
+    fish species as fishSpeciesIn
+    fish weight as fishWeightIn
+    fish length as fishLengthIn
+    This is important for translation on server side
+    '''
+
+    def textPackage(self, nameIn, fishSpeciesIn, fishWeightIn, fishLengthIn):
+            #Peace of mind convertions to float values
+            fishWeightIn = int(fishWeightIn)
+            fishLengthIn = int(fishLengthIn)
+            '''
+            #Convert to bytes for np
+            nameIn = str.encode(nameIn)
+            fishSpeciesIn = str.encode(fishSpeciesIn)
+
+            #Checking input variable types
+            if (type(fishWeightIn) is int) == False:
+                typeChecks = True
+                print('Incorrect Type fishWeightIn')
+                #Make this produce an error code
+            elif (type(fishWeightIn) is int) == True:
+                typeChecks = False
+                fishWeightIn = bytes([fishWeightIn])
+                print('Correct Type fishWeightIn')
+
+            if (type(fishLengthIn) is int) == False:
+                typeChecks = True
+                print('Incorrect Type fishLengthIn')
+                #Make this produce an error code
+            elif (type(fishLengthIn) is int) == True:
+                typeChecks = False
+                fishLengthIn = bytes([fishLengthIn])
+                print('Correct Type fishLengthIn')
+            '''
+            Name = ['f_name', nameIn]
+            fishSpecies = ['fishName', fishSpeciesIn]
+            fishWeight = ['fishWeight', fishWeightIn]
+            fishLength = ['fishLength', fishLengthIn]
+
+            with open('WRFT.csv', 'w', newline='') as csvfile:
+                lineWriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                lineWriter.writerow(Name)
+                lineWriter.writerow(fishSpecies)
+                lineWriter.writerow(fishWeight)
+                lineWriter.writerow(fishLength)
+                #Call the below to change the specified information
+                #csvPackage(fName, sName, fishSpecies, fishWeight, fishLength)
+
+    def build(self):
+        return kivyFile
+
+if __name__ == "__main__":
+    MainApp().run()
